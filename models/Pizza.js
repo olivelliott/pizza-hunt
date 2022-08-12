2917646822e25b44607a296c3796d49011d2b186
Olive <olivelliott48@gmail.com>
2022-08-12 16:06:55 -0400

    Create model associations and virtuals

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,39 @@
 const { Schema, model } = require('mongoose');
 
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
+    {    
+    toJSON: {
+        virtuals: true
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+    id: false
+    }
+);
+
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 const Pizza = model('Pizza', PizzaSchema);
