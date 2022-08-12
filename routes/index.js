579497896158cdb88b2b3f4231803ee5ae9e359d
Olive <olivelliott48@gmail.com>
2022-08-12 14:53:08 -0400

    Create Pizza api routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
+const apiRoutes = require('./api')
 
 router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
